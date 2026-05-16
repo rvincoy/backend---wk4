5f2ba6dcf1333bb3360907d25ec0d4c2d2b45f85
rvincoy <vin23002@byui.edu>
2026-05-16 16:13:41 -0500

    update swagger.js

--- swagger.js
+++ swagger.js
@@ -3,7 +3,7 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'My API',
-    description: 'Temple API'
+    description: 'Contacts API'
   },
   host: 'backend-wk4.onrender.com',
   schemes: ['https']
